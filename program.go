5ca6c04f472480dc380129df91d779e9530ce4ce
axxx307 <xuj1337@gmail.com>
2018-05-23 17:59:57 +0700

    add counting maxis for each frame

--- program.go
+++ program.go
@@ -20,24 +20,27 @@ func main() {
 	if werr != nil {
 		log.Fatal(werr)
 	}
-	data := wav.GetMonoData()
+	//file bits
+	monoData := wav.GetMonoData()
 
-	s := &stft.STFT{
+	spgramConfig := &stft.STFT{
 		FrameShift: int(float64(wav.SampleRate) / 100.0), // 0.01 sec,
 		FrameLen:   2048,
 		Window:     window.CreateHanning(2048),
 	}
 
-	spectrogram, _ := gossp.SplitSpectrogram(s.STFT(data))
-	PrintMatrixAsGnuplotFormat(spectrogram)
+	spectrogram, _ := gossp.SplitSpectrogram(spgramConfig.STFT(monoData))
+	fingerprints := findPeaksInFrame(spectrogram)
+	fmt.Println(fingerprints)
 }
 
 //PrintMatrixAsGnuplotFormat s
-func PrintMatrixAsGnuplotFormat(matrix [][]float64) {
-	fmt.Println("#", len(matrix[0]), len(matrix)/2)
-	for _, vec := range matrix {
+func findPeaksInFrame(matrix [][]float64) [][]float64 {
+	fmt.Println(len(matrix))
+	frameToFreq := make([][]float64, len(matrix))
+	for frame, vec := range matrix {
 		_, _, _, maxv := peakdetect.PeakDetect(vec[:], 1.0)
-		fmt.Println(maxv)
-		fmt.Println("")
+		frameToFreq[frame] = maxv
 	}
+	return frameToFreq
 }
